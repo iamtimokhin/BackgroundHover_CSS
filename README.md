857b7909d8a98ce302f8a6126a945a97ec6f3e23
iamtimokhin <philipp.timokhin@gmail.com>
2021-07-21 14:52:28 +0300

    BackgroundHover_CSS

---
+++ README.md
@@ -0,0 +1,20 @@
+# BackgroundHover_CSS
+
+### BackgroundHover_CSS🐱‍👤✌✨!
+
+### Live Demo: [please, tap here if u wanna join live demo](https://iamtimokhin.github.io/BackgroundHover_CSS/)👌
+
+### Stack technologies:
+
+- HTML
+- CSS
+- ReactJS
+- Canva
+
+## Preview BackgroundHover_CSS✨
+
+![enter image description here](https://github.com/iamtimokhin/BackgroundHover_CSS/blob/master/src/screenshot/background.gif?raw=true)
+
+## Screenshot BackgroundHover_CSS✨
+
+![enter image description here](https://github.com/iamtimokhin/BackgroundHover_CSS/blob/master/src/screenshot/1.gif?raw=true)
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="box">
+        <span className="box__text">Heading</span>
+      </div>
+      <div className="box1">
+        <span className="box1__text">Heading</span>
+      </div>
+      <div className="box2">
+        <span className="box2__text">Heading</span>
+      </div>
+      <div className="box3">
+        <span className="box3__text">Heading</span>
+      </div>
+      <div className="box4">
+        <span className="box4__text">Heading</span>
+      </div>
+      <div className="box5">
+        <span className="box5__text">Heading</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
